c7743f59433fdcbb46fe0823af596fc30af2c8cd
Jabel Tiago Martins <tiago.jb@hotmail.com>
2020-08-22 14:24:51 -0300

    Adiciona estrutura incial do projeto

---
+++ src/main/java/com/example/simplevoteapi/SimpleVoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplevoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleVoteApiApplication.class, args);
+	}
+
+}
